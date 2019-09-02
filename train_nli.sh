31dba75f0794c69e66d1290264a0b14fb3c5146a
Lixin Su <sulixinict@gmail.com>
2019-09-02 17:39:26 +0800

    ckeckpoint nli passed

---
+++ train_nli.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python train_nli.py --vocab_file uncased_L-12_H-768_A-12/vocab.txt \
+                --bert_config_file configs/bert_config.json \
+                --init_checkpoint uncased_L-12_H-768_A-12/pytorch_model.bin \
+                --output_dir saved_test_nli \
+                --tasks mnli,wnli,snli,scitail,rte,mrpc,qqp,qnli \
+                --do_lower_case \
+                --do_train \
+                --do_eval \
+                --seed 1123 \
+                --max_seq_length 128 \
+                --train_batch_size 32 \
+                --data_dir ../match_dataset/ \
+                --sample prop \
+                --num_train_epochs 3 \
+                
+# mnli,wnli,snli,scitail
+# rte,mrpc,qqp,qnli
