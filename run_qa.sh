7f6938791141916ab785d54eb53888adf9ced0a8
lixinsu <sulixinict@gmail.com>
2019-11-21 12:24:46 +0800

    add bash script for mrc

---
+++ run_qa.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python train.py --vocab_file uncased_L-12_H-768_A-12/vocab.txt \
+                --config_file configs/bert_config.json \
+                --init_checkpoint uncased_L-12_H-768_A-12/pytorch_model.bin \
+                --output_dir experiment/mrqa/ \
+                --tasks $1 \
+                --source bert \
+                --data_dir dataset/\
+                --do_lower_case \
+                --do_train \
+                --do_predict
+                 
